c831096ac078bf9aa5db079d36f8fc467b0501fe
Jagannath Sahoo <111991985+Jaggu5040@users.noreply.github.com>
2026-08-06 16:31:31 +0530

    Update v1

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
        await mongoose.connect(process.env.MONGO_URL, {
-        // dbName: 'Production'
-        dbName: 'Development'
+        dbName: 'Production'
+        // dbName: 'Development'
        });
        console.log('MongoDB connected successfully');   
     } catch (error) {
